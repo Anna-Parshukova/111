6115b8c5d4c9db8b635af2021a8f829ea7124f03
Anna Parshukova <annpaarshukova041@gmail.com>
2024-07-28 22:53:04 +0300

    nvn

--- src/main.js
+++ src/main.js
@@ -1,27 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const coversSection = document.getElementById('covers-section');
-    const coversList = document.querySelector('.covers');
+  const coversSection = document.getElementById('covers-section');
+  const marqueeInner = document.querySelectorAll('.marquee__inner');
 
-    function isInViewport(element) {
-        const rect = element.getBoundingClientRect();
-        return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
+  function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function checkVisibility() {
+    if (isInViewport(coversSection)) {
+      marqueeInner.forEach(inner => inner.classList.add('moving'));
+    } else {
+      marqueeInner.forEach(inner => inner.classList.remove('moving'));
     }
+  }
 
-    function checkVisibility() {
-        if (isInViewport(coversSection)) {
-            coversList.classList.add('moving');
-        } else {
-            coversList.classList.remove('moving');
-        }
-    }
+  window.addEventListener('scroll', checkVisibility);
+  window.addEventListener('resize', checkVisibility);
 
-    window.addEventListener('scroll', checkVisibility);
-    window.addEventListener('resize', checkVisibility);
-
-    checkVisibility();
+  checkVisibility();
 });
